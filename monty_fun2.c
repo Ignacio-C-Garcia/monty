158dbe47ecb90e5bf93ea0841cd3c9c237fe087f
ojo-perezoso <bymartin24@gmail.com>
2022-01-12 16:41:05 -0800

    broke div on porpouse

--- monty_fun2.c
+++ monty_fun2.c
@@ -55,11 +55,11 @@ void monty_div(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n == 0)
+	/*if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
-	}
+	}*/
 	(*stack)->next->n /= (*stack)->n;
 	monty_pop(stack, line_number);
 }
